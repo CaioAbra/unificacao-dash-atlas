1191c259ec42274e805a2035ca93db64b6790840
CaioAbra <caioabra41@gmail.com>
2024-10-09 22:25:36 -0300

    ajuste grafico dash

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -411,3 +411,28 @@ var options1 = {
 
 var chart1 = new ApexCharts(document.querySelector("#chart1"), options1);
 chart1.render();
+
+
+$(document).ready(function () {
+    var ctx = document.getElementById('chartDash').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set'],
+            datasets: [{
+                label: 'Receita',
+                data: [10000, 12000, 14000, 13000, 15000, 16000, 18000, 20000, 22000],
+                backgroundColor: '#DE8734',
+                borderColor: '#DE8734',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
