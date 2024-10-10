c6d56ed6c26b0912006e46f1a81dc2ef5a9f10fb
CaioAbra <caioabra41@gmail.com>
2024-10-09 23:16:25 -0300

    ajuste links

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -436,3 +436,39 @@ $(document).ready(function () {
         }
     });
 });
+
+
+//carousel
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'horizontal',
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+
+    speed: 600,
+    previousClicks: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 300,
+        modifier: 2.5,
+    },
+    // // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.nex',
+        prevEl: '.pre',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
